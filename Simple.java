aa37964e27deaaf2a5c983b3669d777e2c1f750b
API1288 <86962985+API1288@users.noreply.github.com>
2021-07-07 17:15:32 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -7,5 +7,7 @@ class Simple{
 		System.out.println("5th change");
 		System.out.println("6th change");
 		System.out.println("7th change");
+	    	System.out.println("8th change");
+	    
     }  
 }  
