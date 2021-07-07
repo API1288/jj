aedde9050307b12a9a8d8ecf5ca27c2148b42c91
API1288 <suparna.karmakar@apisero.com>
2021-07-07 16:16:30 +0530

    committed

---
+++ Simple.java
@@ -0,0 +1,9 @@
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+	System.out.println("Hello git");
+	    System.out.println("demo github");
+		System.out.println("4th change");
+		System.out.println("5th change");
+    }  
+}  
